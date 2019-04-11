8514997c69a71d484f092858ede7c9280e1b9c35
Katie Carrel <katiecarrel@comcast.net>
2019-04-11 13:28:43 -0700

    sad

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import Navbar from './Containers/Navbar'
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Navbar />
       </div>
     );
   }
