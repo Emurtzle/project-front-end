6919b016a58e6538c598517d2cd59d162108f03d
Emily Wagner <EmilyWagner598@gmail.com>
2019-04-12 13:52:47 -0700

    For katie <3

--- project-front-end/src/Containers/NavigationBar.js
+++ project-front-end/src/Containers/NavigationBar.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import 'react-bulma-components/dist/react-bulma-components.min.css'
+import { Navbar, Button } from 'react-bulma-components';
+import logo from '../logo.png'
 
 class NavigationBar extends Component {
     constructor(props) {
@@ -11,7 +14,28 @@ class NavigationBar extends Component {
     render() {
         return (
             <div>
-                
+                <Navbar color={"primary"} fixed={"top"} active={false} transparent={false}>
+                    
+                    <Navbar.Brand>
+                        <Navbar.Item renderAs="a" href="/">
+                            <img src={logo} alt="Panning Out Logo" width="80"/>
+                        </Navbar.Item>
+                        <Navbar.Burger />
+                    </Navbar.Brand>
+
+                    <Navbar.Menu>
+                        <Navbar.Container>
+                            <Navbar.Item href="/">Closet</Navbar.Item>
+                            <Navbar.Item href="/">Inspiration</Navbar.Item>
+                        </Navbar.Container>
+
+                        <Navbar.Container position="end">
+                            <Navbar.Item href="/">Profile</Navbar.Item>
+                            <Navbar.Item href="/">Log In</Navbar.Item>
+                        </Navbar.Container>
+
+                    </Navbar.Menu>
+                </Navbar>
             </div>
         )
     }
