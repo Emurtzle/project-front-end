10d57971d1f9f1bf0d6f0d3f76a9bb51e7b54ef6
Emily Wagner <EmilyWagner598@gmail.com>
2019-04-12 09:25:22 -0700

    Safety first

--- project-front-end/src/App.js
+++ project-front-end/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import NavigationBar from './Containers/NavigationBar'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <NavigationBar />
       </div>
     );
   }
