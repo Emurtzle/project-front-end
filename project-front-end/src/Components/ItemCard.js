8fbfcaab12c1f02263ec7abcd4cba20d65512127
Emily Wagner <EmilyWagner598@gmail.com>
2019-04-12 18:01:23 -0700

    Got card list

---
+++ project-front-end/src/Components/ItemCard.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import { Card } from 'react-bulma-components'
+import 'react-bulma-components/dist/react-bulma-components.min.css'
+import makeupIcon from '../makeupIcon.png'
+
+class ItemCard extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            active: false
+        }
+    }
+    render() {
+        let {name, brand, expiration} = this.props.item
+
+        return(
+            <Card className={"has-background-grey-light"}>
+                <Card.Image size={64} src={makeupIcon} alt="Makeup icon type"/>
+                <Card.Content>
+                    <Card.Header.Title>{name}</Card.Header.Title>
+                    <Card.Header>{brand}</Card.Header>
+                    <Card.Header>Expires: {expiration}</Card.Header>
+                </Card.Content>
+                <Card.Footer >
+                    <Card.Footer.Item renderAs="a" href="#edit">
+                        Edit
+                    </Card.Footer.Item>
+                    <Card.Footer.Item renderAs="a" href="#remove">
+                        Remove
+                    </Card.Footer.Item>
+                </Card.Footer>
+            </Card>
+        )
+    }
+}
+
+export default ItemCard;
--- project-front-end/src/Containers/Collection.js
+++ project-front-end/src/Containers/Collection.js
@@ -1,30 +1,60 @@
 import React, { Component } from 'react'
-import { Card, Container, Heading } from 'react-bulma-components'
+import { Container, Heading } from 'react-bulma-components'
 import 'react-bulma-components/dist/react-bulma-components.min.css'
-import makeupIcon from '../makeupIcon.png'
+import ItemCard from '../Components/ItemCard'
 
 class Collection extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state ={
+            items: [
+                {
+                    name: "Blue Eagle",
+                    brand: "Aspire",
+                    expiration: "tuesday"
+                },
+                {
+                    name: "Sparkle Planet",
+                    brand: "Lust Mechanic",
+                    expiration: "Next Month"
+                },
+                {
+                    name: "Butterfly Dust",
+                    brand: "Lust Mechanic",
+                    expiration: "Next Week"
+                },
+                {
+                    name: "City Nights",
+                    brand: "Sephora",
+                    expiration: "Tomorrow"
+                },
+                {
+                    name: "Spring Days",
+                    brand: "Ulta",
+                    expiration: "Thursday"
+                },
+                {
+                    name: "Nightclub",
+                    brand: "Cover Girl",
+                    expiration: "now"
+                }
+            ]
+        }
+    }
+
+    loadItems = () => {
+        return this.state.items.map((item, index) => (
+            <ItemCard item={item} key={index} />
+        ))
+    }
+
     render() {
         return (
             <Container fluid className={"has-background-info"}>
                 <Heading className={"has-text-centered has-text-white"}>Collection</Heading>
                 <Container fluid className={"has-background-link"}>
-                <Card className={"has-background-success"}>
-                    <Card.Image size={64} src={makeupIcon} alt="Makeup icon type"/>
-                    <Card.Content>
-                        <Card.Header.Title>Makeup Name</Card.Header.Title>
-                        <Card.Header>Makeup Brand</Card.Header>
-                        <Card.Header>Makeup Expiration</Card.Header>
-                    </Card.Content>
-                    <Card.Footer >
-                        <Card.Footer.Item renderAs="a" href="#edit">
-                            Edit
-                        </Card.Footer.Item>
-                        <Card.Footer.Item renderAs="a" href="#edit">
-                            Delete
-                        </Card.Footer.Item>
-                    </Card.Footer>
-                </Card>
+                    {this.loadItems()}
                 </Container>
             </Container>
         )
